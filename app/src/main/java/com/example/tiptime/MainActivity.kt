12b7c0fb5e502c1e5de0a5674bce2d4a216f3236
Andres Martinez <andres_martinez-16@hotmail.com>
2022-06-09 12:55:38 -0500

    Cuarto commite: Se elimina codigo inesesario y se crea una funcion para el codigo repetitivo

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,18 +17,18 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener{calculateTip()}
 
     }
-    fun calculateTip(){
+    private fun calculateTip(){
 
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null){
+        if (cost == null || cost == 0.0){
 
+            displayTip(0.0)
             return
 
         }
 
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercent = when(selectedId){
+        val tipPercent = when(binding.tipOptions.checkedRadioButtonId){
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -36,13 +36,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         var tip = cost * tipPercent
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp){
+        if (binding.roundUpSwitch.isChecked){
             tip = kotlin.math.ceil(tip)
         }
 
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text= getString(R.string.tip_amount, formattedTip)
+        displayTip(tip)
 
+    }
+    private fun displayTip(tip : Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
